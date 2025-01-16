1f0839a3250acd79613be918fc03a8ee91088f36
Ethan Johnston <rocketstrong600@gmail.com>
2025-01-17 01:30:25 +1000

    organise a few things

--- src/device.rs
+++ src/device.rs
@@ -1,14 +1,103 @@
-use ash::{vk, Device, Entry, Instance};
+use ash::{vk, Instance};
+use std::error;
 
-fn select_vk_physical_device(instance: &Instance) -> vk::PhysicalDevice {
-    let physical_devices_result = unsafe { instance.enumerate_physical_devices() };
-    let physical_devices = match physical_devices_result {
-        Ok(physical_devices) => physical_devices,
-        Err(error) => panic!("Error Getting Devices: {error:?}"),
-    };
+// we dyn box the error in result to make error inference runtime,
+// this is so that the function can support multiple error types.
+// these errors are passed on by different functions using ?
+pub fn select_vk_physical_device(
+    instance: &Instance,
+) -> Result<vk::PhysicalDevice, Box<dyn error::Error>> {
+    let physical_devices = unsafe { instance.enumerate_physical_devices()? };
 
-    let p_device_index_score: (u32, u32) = (0, 0);
-    for (index, p_device) in physical_devices.iter().enumerate() {
-        // todo
+    let physical_device = *physical_devices
+        .iter()
+        .max_by_key(|physical_device| score_physical_device(physical_device, instance))
+        .ok_or("Devices Vector Empty")?;
+    Ok(physical_device)
+}
+
+// calculate a capability score for a physical device
+// score improvment should go down as importance of property goes down
+fn score_physical_device(physical_device: &vk::PhysicalDevice, instance: &Instance) -> u64 {
+    let mut score: u64 = 0;
+    let device_properties = unsafe { instance.get_physical_device_properties(*physical_device) };
+
+    // llvmpipe virtual gpu can go die in a hole
+    if device_properties
+        .device_name_as_c_str()
+        .unwrap_or_default()
+        .to_str()
+        .unwrap_or_default()
+        .starts_with("llvmpipe")
+    {
+        return 0;
     }
+    // discrete gpu adds more score than integrated everything else does not improve score
+    let device_type = device_properties.device_type;
+    match device_type {
+        vk::PhysicalDeviceType::DISCRETE_GPU => {
+            score += 100;
+        }
+        vk::PhysicalDeviceType::INTEGRATED_GPU => {
+            score += 50;
+        }
+        _ => {}
+    }
+
+    let device_features = unsafe { instance.get_physical_device_features(*physical_device) };
+
+    let queue_family_properties =
+        unsafe { instance.get_physical_device_queue_family_properties(*physical_device) };
+
+    // you can't even make a game without a graphics queue
+    let graphics_queue = queue_family_properties
+        .iter()
+        .any(|queue_prop| queue_prop.queue_flags.contains(vk::QueueFlags::GRAPHICS));
+
+    // good cards should be capable of compute
+    let compute_queue = queue_family_properties
+        .iter()
+        .any(|queue_prop| queue_prop.queue_flags.contains(vk::QueueFlags::COMPUTE));
+
+    if graphics_queue {
+        score += 30
+    }
+
+    if compute_queue {
+        score += 10
+    }
+
+    // Cards with Geometry shaders are typically newer
+    if device_features.geometry_shader == vk::TRUE {
+        score += 10
+    }
+
+    // 64 bit floats is not common on low end cards?
+    if device_features.shader_float64 == vk::TRUE {
+        score += 5
+    }
+
+    // add gpu memory to score devices with higer vram tend to be better.
+    // capped at 64gb to filter out devices with querks
+    score += (physical_device_memory_size(physical_device, instance) / 1024).min(64);
+    score
+}
+
+// get device memory in MiB
+pub fn physical_device_memory_size(
+    physical_device: &vk::PhysicalDevice,
+    instance: &Instance,
+) -> u64 {
+    let memory_properties =
+        unsafe { instance.get_physical_device_memory_properties(*physical_device) };
+    memory_properties
+        .memory_heaps
+        .iter()
+        .fold(0u64, |acc, heap| {
+            if heap.flags.contains(vk::MemoryHeapFlags::DEVICE_LOCAL) {
+                acc + heap.size / (1024 * 1024) // Convert to MiB
+            } else {
+                acc
+            }
+        })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use ash::{vk, Entry};
 mod device;
+use ash::{vk, Entry};
+use device::{physical_device_memory_size, select_vk_physical_device};
+use std::ffi::CStr;
 
 fn main() {
     // Load Vulkan Library
@@ -29,6 +31,42 @@ fn main() {
         Ok(instance) => instance,
         Err(error) => panic!("Failed to Create Vulkan Instance: {error:?}"),
     };
+
+    let physical_device_result = select_vk_physical_device(&instance);
+
+    let physical_device = match physical_device_result {
+        Ok(physical_device) => physical_device,
+        Err(error) => panic!("Failed to pick device: {error:?}"),
+    };
+
+    let instance_version = unsafe {
+        instance
+            .get_physical_device_properties(physical_device)
+            .api_version
+    };
+
+    let device_name = unsafe {
+        CStr::from_ptr(
+            instance
+                .get_physical_device_properties(physical_device)
+                .device_name
+                .as_ptr(),
+        )
+        .to_string_lossy()
+    };
+
+    let major = vk::api_version_major(instance_version);
+    let minor = vk::api_version_minor(instance_version);
+    let patch = vk::api_version_patch(instance_version);
+
+    println!(
+        "Device Name: {}\nVulkan Instance Version: {}.{}.{}",
+        device_name, major, minor, patch
+    );
+    println!(
+        "Device Memory: {}MiB",
+        physical_device_memory_size(&physical_device, &instance)
+    );
 
     //Cleanup vulkan instance
     unsafe { instance.destroy_instance(None) };
