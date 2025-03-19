8a775fe55b2c755332fb58d6f2542faab3d02041
Ethan Johnston <rocketstrong600@gmail.com>
2025-03-20 01:18:15 +1000

    Move Rendering from app to VKRender for future organisation

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,7 @@
-use crate::renderer::device::VKDevice;
-use crate::renderer::presentation::VKPresent;
 use crate::renderer::VKContext;
+use crate::renderer::VKRenderer;
 use crate::utils::GameInfo;
 use crate::utils::ReplaceWith;
-use ash::vk;
 use log::info;
 use winit::application::ApplicationHandler;
 use winit::error::EventLoopError;
@@ -18,9 +16,7 @@ use winit::window::WindowId;
 pub struct AppCTX<'a> {
     pub game_info: GameInfo,
     pub window: Window,
-    pub vulkan_ctx: VKContext<'a>,
-    pub vulkan_present: VKPresent,
-    pub vulkan_cmd_buffs: Vec<vk::CommandBuffer>,
+    pub vulkan_renderer: VKRenderer<'a>,
 }
 
 impl AppCTX<'_> {
@@ -31,42 +27,15 @@ impl AppCTX<'_> {
             )
             .unwrap();
 
-        let frames_in_flight = 2;
-
         let vulkan_ctx = VKContext::new(&game_info, &window).unwrap();
-        let vulkan_present = unsafe {
-            VKPresent::default()
-                .max_frames(frames_in_flight, &vulkan_ctx)
-                .unwrap()
-        };
 
-        // allocate 1 primary command buffer
-        let alloc_info = vk::CommandBufferAllocateInfo::default()
-            .command_pool(vulkan_ctx.vulkan_cmd_pool)
-            .command_buffer_count(frames_in_flight)
-            .level(vk::CommandBufferLevel::PRIMARY);
-
-        let vulkan_cmd_buffs = unsafe {
-            vulkan_ctx
-                .vulkan_device
-                .device
-                .allocate_command_buffers(&alloc_info)
-                .unwrap()
-        };
+        let vulkan_renderer = VKRenderer::new(vulkan_ctx, 2).unwrap();
 
         Self {
             game_info,
             window,
-            vulkan_ctx,
-            vulkan_present,
-            vulkan_cmd_buffs,
+            vulkan_renderer,
         }
-    }
-}
-
-impl Drop for AppCTX<'_> {
-    fn drop(&mut self) {
-        unsafe { self.vulkan_present.destroy(&self.vulkan_ctx) };
     }
 }
 
@@ -100,7 +69,7 @@ impl ApplicationHandler for App<'_> {
             }
             WindowEvent::RedrawRequested => {
                 if let App::Initialised(app_ctx) = self {
-                    render(app_ctx);
+                    app_ctx.vulkan_renderer.render(&app_ctx.window);
                     app_ctx.window.request_redraw();
                 }
             }
@@ -136,123 +105,4 @@ impl App<'_> {
         event_loop.set_control_flow(ControlFlow::Poll);
         event_loop.run_app_on_demand(self)
     }
-}
-
-fn render(app_ctx: &mut AppCTX) {
-    let vk_ctx = &app_ctx.vulkan_ctx;
-    let vk_present = &mut app_ctx.vulkan_present;
-    let vk_device = &vk_ctx.vulkan_device;
-
-    let render_info = vk_present.aquire_img(vk_ctx).unwrap();
-
-    unsafe {
-        record_cmd_buffer(
-            app_ctx.vulkan_cmd_buffs[render_info.frame_in_flight as usize],
-            vk_device,
-            vk_ctx.vulkan_swapchain.images[render_info.img_aquired_index as usize],
-        )
-        .unwrap();
-    }
-
-    let command_buffer_infos = &[vk::CommandBufferSubmitInfo::default()
-        .command_buffer(app_ctx.vulkan_cmd_buffs[render_info.frame_in_flight as usize])];
-
-    let wait_semaphore_infos = &[vk::SemaphoreSubmitInfo::default()
-        .semaphore(render_info.img_aquired_gpu)
-        .stage_mask(vk::PipelineStageFlags2::TRANSFER)];
-
-    let signal_semaphore_infos = &[vk::SemaphoreSubmitInfo::default()
-        .semaphore(render_info.done_rendering_gpu)
-        .stage_mask(vk::PipelineStageFlags2::BOTTOM_OF_PIPE)];
-
-    let submits = [vk::SubmitInfo2::default()
-        .wait_semaphore_infos(wait_semaphore_infos)
-        .signal_semaphore_infos(signal_semaphore_infos)
-        .command_buffer_infos(command_buffer_infos)];
-
-    unsafe {
-        vk_device
-            .device
-            .queue_submit2(
-                vk_device.graphics_queue,
-                &submits,
-                render_info.done_rendering_cpu,
-            )
-            .unwrap()
-    };
-
-    // required for wayland
-    app_ctx.window.pre_present_notify();
-
-    vk_present.present_frame(vk_ctx).unwrap();
-}
-
-unsafe fn record_cmd_buffer(
-    cmd_buffer: ash::vk::CommandBuffer,
-    vk_device: &VKDevice,
-    image: ash::vk::Image,
-) -> Result<(), ash::vk::Result> {
-    let begin_info = vk::CommandBufferBeginInfo::default();
-
-    let sub_resource_range = vk::ImageSubresourceRange::default()
-        .aspect_mask(vk::ImageAspectFlags::COLOR)
-        .level_count(1)
-        .layer_count(1);
-
-    // memory barriar info for clear
-    // we use memory barriars to transistion the image into the correct layout
-    // this is for transitioning the layout to the required layout for screen clear cmd
-    let image_memory_barriers = [vk::ImageMemoryBarrier2::default()
-        .old_layout(vk::ImageLayout::UNDEFINED)
-        .new_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
-        .src_stage_mask(vk::PipelineStageFlags2::TOP_OF_PIPE)
-        .dst_stage_mask(vk::PipelineStageFlags2::TRANSFER)
-        .dst_access_mask(vk::AccessFlags2::TRANSFER_WRITE)
-        .image(image)
-        .subresource_range(sub_resource_range)];
-
-    // memory barriar info for present
-    // we use memory barriars to transistion the image into the correct layout
-    // this is for the final layout before presenting
-    let present_image_memory_barriers = [vk::ImageMemoryBarrier2::default()
-        .old_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
-        .new_layout(vk::ImageLayout::PRESENT_SRC_KHR)
-        .src_stage_mask(vk::PipelineStageFlags2::TRANSFER)
-        .dst_stage_mask(vk::PipelineStageFlags2::BOTTOM_OF_PIPE)
-        .src_access_mask(vk::AccessFlags2::TRANSFER_WRITE_KHR)
-        .image(image)
-        .subresource_range(sub_resource_range)];
-
-    // nice pinky colour
-    let mut clear_color_value = vk::ClearColorValue::default();
-    clear_color_value.float32 = [0.74757, 0.02016, 0.253, 1.0];
-
-    let dependency_info =
-        vk::DependencyInfo::default().image_memory_barriers(&image_memory_barriers);
-
-    let present_dependency_info =
-        vk::DependencyInfo::default().image_memory_barriers(&present_image_memory_barriers);
-
-    vk_device
-        .device
-        .begin_command_buffer(cmd_buffer, &begin_info)
-        .unwrap();
-
-    vk_device
-        .device
-        .cmd_pipeline_barrier2(cmd_buffer, &dependency_info);
-
-    vk_device.device.cmd_clear_color_image(
-        cmd_buffer,
-        image,
-        vk::ImageLayout::TRANSFER_DST_OPTIMAL,
-        &clear_color_value,
-        &[sub_resource_range],
-    );
-
-    vk_device
-        .device
-        .cmd_pipeline_barrier2(cmd_buffer, &present_dependency_info);
-
-    vk_device.device.end_command_buffer(cmd_buffer)
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -3,6 +3,7 @@ pub mod presentation;
 pub mod shader;
 
 use crate::renderer::device::VKDevice;
+use crate::renderer::presentation::VKPresent;
 use crate::utils::GameInfo;
 use ash::vk::ShaderStageFlags;
 use ash::{vk, Entry, Instance};
@@ -82,88 +83,34 @@ impl VKInstance {
 }
 
 //Safe Destruction Order structs drop from top to bottom.
-pub struct VKContext<'a> {
-    pub vulkan_cmd_pool: vk::CommandPool, // TODO: probably move out of here to something that magages the actual rendering algo
-
-    pub vertex_shader: VKShader<'a>,
-    pub fragment_shader: VKShader<'a>,
-
-    pub vulkan_shader_loader: VKShaderLoader<&'static str>,
+pub struct VKContext {
     pub vulkan_swapchain: VKSwapchain,
     pub vulkan_surface: VKSurface,
     pub vulkan_device: VKDevice,
     pub vulkan_instance: VKInstance,
 }
 
-impl VKContext<'_> {
+impl VKContext {
     pub fn new(game_info: &GameInfo, window: &Window) -> Result<Self, Box<dyn error::Error>> {
         let vk_instance_ext = display_vk_ext(window)?;
         let vulkan_instance = VKInstance::new(game_info, Some(vk_instance_ext))?;
         let vulkan_surface = VKSurface::new(&vulkan_instance, window)?;
         let vulkan_device = VKDevice::new(&vulkan_instance, &vulkan_surface)?;
         let vulkan_swapchain = VKSwapchain::new(&vulkan_instance, &vulkan_device, &vulkan_surface)?;
-        let mut vulkan_shader_loader = VKShaderLoader::default();
-
-        let cmd_pool_info = vk::CommandPoolCreateInfo::default()
-            .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
-            .queue_family_index(vulkan_device.queue_index);
-
-        // Create Command Pool
-        let vulkan_cmd_pool = unsafe {
-            vulkan_device
-                .device
-                .create_command_pool(&cmd_pool_info, None)?
-        };
-
-        let vertex_shader = VKShader::new(
-            &vulkan_device,
-            "shaders/triangle.spv",
-            ShaderStageFlags::VERTEX,
-            c"vertexMain",
-            &mut vulkan_shader_loader,
-        )?;
-
-        let fragment_shader = VKShader::new(
-            &vulkan_device,
-            "shaders/triangle.spv",
-            ShaderStageFlags::FRAGMENT,
-            c"fragMain",
-            &mut vulkan_shader_loader,
-        )?;
 
         Ok(Self {
             vulkan_instance,
             vulkan_device,
             vulkan_surface,
             vulkan_swapchain,
-            vulkan_shader_loader,
-            vertex_shader,
-            fragment_shader,
-            vulkan_cmd_pool,
         })
     }
-}
 
-impl Drop for VKContext<'_> {
-    fn drop(&mut self) {
-        unsafe {
-            self.vulkan_device
-                .device
-                .device_wait_idle()
-                .unwrap_unchecked();
-
-            self.vulkan_device
-                .device
-                .destroy_command_pool(self.vulkan_cmd_pool, None);
-
-            self.fragment_shader.destroy(&self.vulkan_device);
-            self.vertex_shader.destroy(&self.vulkan_device);
-
-            self.vulkan_swapchain.destroy(&self.vulkan_device);
-            self.vulkan_surface.destroy();
-            self.vulkan_device.destroy();
-            self.vulkan_instance.destroy();
-        }
+    pub unsafe fn destroy(&mut self) {
+        self.vulkan_swapchain.destroy(&self.vulkan_device);
+        self.vulkan_surface.destroy();
+        self.vulkan_device.destroy();
+        self.vulkan_instance.destroy();
     }
 }
 
@@ -173,4 +120,222 @@ pub fn display_vk_ext(window: &Window) -> Result<&'static [*const c_char], Box<d
     Ok(ash_window::enumerate_required_extensions(
         display_handle.as_raw(),
     )?)
+}
+
+pub struct VKRenderer<'a> {
+    pub vulkan_ctx: VKContext,
+    pub vulkan_shader_loader: VKShaderLoader<&'static str>,
+    pub vulkan_present: VKPresent,
+
+    pub vulkan_cmd_pool: vk::CommandPool,
+    pub vulkan_cmd_buffs: Vec<vk::CommandBuffer>,
+    pub vertex_shader: VKShader<'a>,
+    pub fragment_shader: VKShader<'a>,
+}
+
+impl VKRenderer<'_> {
+    pub fn new(
+        vulkan_ctx: VKContext,
+        frames_in_flight: u32,
+    ) -> Result<Self, Box<dyn error::Error>> {
+        let vulkan_present = unsafe {
+            VKPresent::default()
+                .max_frames(frames_in_flight, &vulkan_ctx)
+                .unwrap()
+        };
+
+        let cmd_pool_info = vk::CommandPoolCreateInfo::default()
+            .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
+            .queue_family_index(vulkan_ctx.vulkan_device.queue_index);
+
+        // Create Command Pool
+        let vulkan_cmd_pool = unsafe {
+            vulkan_ctx
+                .vulkan_device
+                .device
+                .create_command_pool(&cmd_pool_info, None)?
+        };
+
+        // allocate 1 primary command buffer
+        let alloc_info = vk::CommandBufferAllocateInfo::default()
+            .command_pool(vulkan_cmd_pool)
+            .command_buffer_count(frames_in_flight)
+            .level(vk::CommandBufferLevel::PRIMARY);
+
+        let vulkan_cmd_buffs = unsafe {
+            vulkan_ctx
+                .vulkan_device
+                .device
+                .allocate_command_buffers(&alloc_info)
+                .unwrap()
+        };
+
+        let mut vulkan_shader_loader = VKShaderLoader::default();
+        let vertex_shader = VKShader::new(
+            &vulkan_ctx.vulkan_device,
+            "shaders/triangle.spv",
+            ShaderStageFlags::VERTEX,
+            c"vertexMain",
+            &mut vulkan_shader_loader,
+        )?;
+
+        let fragment_shader = VKShader::new(
+            &vulkan_ctx.vulkan_device,
+            "shaders/triangle.spv",
+            ShaderStageFlags::FRAGMENT,
+            c"fragMain",
+            &mut vulkan_shader_loader,
+        )?;
+
+        Ok(Self {
+            vulkan_ctx,
+            vulkan_shader_loader,
+            vulkan_present,
+            vulkan_cmd_pool,
+            vulkan_cmd_buffs,
+            vertex_shader,
+            fragment_shader,
+        })
+    }
+
+    pub fn render(&mut self, window: &Window) {
+        let vk_ctx = &self.vulkan_ctx;
+        let vk_present = &mut self.vulkan_present;
+        let vk_device = &vk_ctx.vulkan_device;
+
+        let render_info = vk_present.aquire_img(vk_ctx).unwrap();
+
+        unsafe {
+            Self::record_cmd_buffer(
+                self.vulkan_cmd_buffs[render_info.frame_in_flight as usize],
+                vk_device,
+                vk_ctx.vulkan_swapchain.images[render_info.img_aquired_index as usize],
+            )
+            .unwrap();
+        }
+
+        let command_buffer_infos = &[vk::CommandBufferSubmitInfo::default()
+            .command_buffer(self.vulkan_cmd_buffs[render_info.frame_in_flight as usize])];
+
+        let wait_semaphore_infos = &[vk::SemaphoreSubmitInfo::default()
+            .semaphore(render_info.img_aquired_gpu)
+            .stage_mask(vk::PipelineStageFlags2::TRANSFER)];
+
+        let signal_semaphore_infos = &[vk::SemaphoreSubmitInfo::default()
+            .semaphore(render_info.done_rendering_gpu)
+            .stage_mask(vk::PipelineStageFlags2::BOTTOM_OF_PIPE)];
+
+        let submits = [vk::SubmitInfo2::default()
+            .wait_semaphore_infos(wait_semaphore_infos)
+            .signal_semaphore_infos(signal_semaphore_infos)
+            .command_buffer_infos(command_buffer_infos)];
+
+        unsafe {
+            vk_device
+                .device
+                .queue_submit2(
+                    vk_device.graphics_queue,
+                    &submits,
+                    render_info.done_rendering_cpu,
+                )
+                .unwrap()
+        };
+
+        // required for wayland
+        window.pre_present_notify();
+
+        vk_present.present_frame(vk_ctx).unwrap();
+    }
+
+    unsafe fn record_cmd_buffer(
+        cmd_buffer: ash::vk::CommandBuffer,
+        vk_device: &VKDevice,
+        image: ash::vk::Image,
+    ) -> Result<(), ash::vk::Result> {
+        let begin_info = vk::CommandBufferBeginInfo::default();
+
+        let sub_resource_range = vk::ImageSubresourceRange::default()
+            .aspect_mask(vk::ImageAspectFlags::COLOR)
+            .level_count(1)
+            .layer_count(1);
+
+        // memory barriar info for clear
+        // we use memory barriars to transistion the image into the correct layout
+        // this is for transitioning the layout to the required layout for screen clear cmd
+        let image_memory_barriers = [vk::ImageMemoryBarrier2::default()
+            .old_layout(vk::ImageLayout::UNDEFINED)
+            .new_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
+            .src_stage_mask(vk::PipelineStageFlags2::TOP_OF_PIPE)
+            .dst_stage_mask(vk::PipelineStageFlags2::TRANSFER)
+            .dst_access_mask(vk::AccessFlags2::TRANSFER_WRITE)
+            .image(image)
+            .subresource_range(sub_resource_range)];
+
+        // memory barriar info for present
+        // we use memory barriars to transistion the image into the correct layout
+        // this is for the final layout before presenting
+        let present_image_memory_barriers = [vk::ImageMemoryBarrier2::default()
+            .old_layout(vk::ImageLayout::TRANSFER_DST_OPTIMAL)
+            .new_layout(vk::ImageLayout::PRESENT_SRC_KHR)
+            .src_stage_mask(vk::PipelineStageFlags2::TRANSFER)
+            .dst_stage_mask(vk::PipelineStageFlags2::BOTTOM_OF_PIPE)
+            .src_access_mask(vk::AccessFlags2::TRANSFER_WRITE_KHR)
+            .image(image)
+            .subresource_range(sub_resource_range)];
+
+        // nice pinky colour
+        let mut clear_color_value = vk::ClearColorValue::default();
+        clear_color_value.float32 = [0.74757, 0.02016, 0.253, 1.0];
+
+        let dependency_info =
+            vk::DependencyInfo::default().image_memory_barriers(&image_memory_barriers);
+
+        let present_dependency_info =
+            vk::DependencyInfo::default().image_memory_barriers(&present_image_memory_barriers);
+
+        vk_device
+            .device
+            .begin_command_buffer(cmd_buffer, &begin_info)
+            .unwrap();
+
+        vk_device
+            .device
+            .cmd_pipeline_barrier2(cmd_buffer, &dependency_info);
+
+        vk_device.device.cmd_clear_color_image(
+            cmd_buffer,
+            image,
+            vk::ImageLayout::TRANSFER_DST_OPTIMAL,
+            &clear_color_value,
+            &[sub_resource_range],
+        );
+
+        vk_device
+            .device
+            .cmd_pipeline_barrier2(cmd_buffer, &present_dependency_info);
+
+        vk_device.device.end_command_buffer(cmd_buffer)
+    }
+}
+
+impl Drop for VKRenderer<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            self.vulkan_ctx
+                .vulkan_device
+                .device
+                .device_wait_idle()
+                .unwrap_unchecked();
+            self.fragment_shader.destroy(&self.vulkan_ctx.vulkan_device);
+            self.vertex_shader.destroy(&self.vulkan_ctx.vulkan_device);
+
+            self.vulkan_present.destroy(&self.vulkan_ctx);
+
+            self.vulkan_ctx
+                .vulkan_device
+                .device
+                .destroy_command_pool(self.vulkan_cmd_pool, None);
+            self.vulkan_ctx.destroy();
+        }
+    }
 }
